7c52bb1ab7bb4d64073588f141843bb917863e73
I'm Frieza Oh Oh Oh <31746868+FranciscoSimaoPerez@users.noreply.github.com>
2018-05-13 23:53:47 +0100

    toggle side nav

---
+++ components/TheSideNavToggle/TheSideNavToggle.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="drawer-toggle" role="button" @click="$emit('toggle')">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+</template>
+
+<style scoped>
+.slide-side-enter-active,
+.slide-side-leave-active {
+  transition: all 0.3s ease-out;
+}
+.slide-side-enter,
+.slide-side-leave-to,{
+  transform: translateX(-100%);
+}
+.drawer-toggle{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    height: 25px;
+    width: 25px;
+    cursor: pointer;
+}
+
+@media (min-width: 768px){
+    .drawer-toggle{
+        display: none;
+    }
+}
+
+.drawer-toggle .bar{
+    width: 90%;
+    height: 2px;
+    background-color: #35495e;
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,39 @@
 <template>
   <div>
+    <the-header @sideNavToggle="displaySideNav = !displaySideNav" />
+    <TheSideNav :show="displaySideNav" @close="displaySideNav=false"/>
     <nuxt/>
   </div>
 </template>
 
+<script>
+import TheHeader from '@/components/TheHeader/TheHeader'
+import TheSideNav from "@/components/TheSideNav/TheSideNav"
+
+export default {
+  components:{
+    TheHeader,
+    TheSideNav
+  },
+  data() {
+    return {
+      displaySideNav: false
+    }
+  },
+}
+</script>
+
 <style>
-html
-{
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+*{
   box-sizing: border-box;
 }
-*, *:before, *:after
-{
-  box-sizing: border-box;
-  margin: 0;
+
+body{
+  font-family: "Lato", sans-serif;
 }
-.button--green
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+
+.main-content {
+  margin-top: 4.5rem;
 }
-.button--green:hover
-{
-  color: #fff;
-  background-color: #3b8070;
-}
-.button--grey
-{
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-.button--grey:hover
-{
-  color: #fff;
-  background-color: #35495e;
-}
+
 </style>
